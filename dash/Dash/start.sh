7d329030deb0308732ec74a89222230599d196bb
Lucas Barrows <lucasbarrows@gmail.com>
2026-02-07 11:38:51 -0300

    asd

---
+++ dash/Dash/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Start nginx in background
+nginx &
+
+# Start Streamlit (foreground)
+streamlit run Home.py \
+    --server.port=8501 \
+    --server.address=127.0.0.1 \
+    --server.headless=true \
+    --server.enableCORS=false \
+    --server.enableXsrfProtection=false \
+    --browser.gatherUsageStats=false
